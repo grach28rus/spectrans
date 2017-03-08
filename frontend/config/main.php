95dad3c7741350920d8108b44874ed687001a115
grach28rus <grach28rus@mail.ru>
2017-03-08 22:55:25 +0300

    add config mailer

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -20,11 +20,11 @@ return [
             'useFileTransport' => false,
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.yandex.ru',
+                'host' => 'mail.yandex.ru',
                 'username' => 'service@territoriyad.com',
                 'password' => '89145924965111',
-                'port' => '465',
-                'encryption' => 'ssl',
+                'port' => '587',
+                'encryption' => 'tls',
             ],
         ],
         'user' => [
